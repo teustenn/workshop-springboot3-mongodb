6f8e8b87346f66b7419a6c34885c1c33a61072f3
teustenn <teustenn@gmail.com>
2022-10-27 19:40:56 -0300

    Exception Handling - findById

---
+++ src/main/java/com/sentinela/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.sentinela.workshopmongo.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sentinela.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError er = new StandardError(System.currentTimeMillis(), status.value(), "NOT FOUND", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(er);	
+	}
+
+}
