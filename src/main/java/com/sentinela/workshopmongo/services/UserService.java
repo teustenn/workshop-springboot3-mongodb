e7a728ddddc553550b857867fbdf177a430d74ff
teustenn <teustenn@gmail.com>
2022-10-27 19:53:52 -0300

    User Insert

--- src/main/java/com/sentinela/workshopmongo/services/UserService.java
+++ src/main/java/com/sentinela/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sentinela.workshopmongo.domain.User;
+import com.sentinela.workshopmongo.dto.UserDTO;
 import com.sentinela.workshopmongo.repositories.UserRepository;
 import com.sentinela.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object NOT FOUND."));
 	}
-
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
