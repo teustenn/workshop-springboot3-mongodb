30d8fc5a5e1dd0cfe60120e712e8fe294c722f50
teustenn <teustenn@gmail.com>
2022-10-27 16:27:15 -0300

    Initial commit

---
+++ src/test/java/com/sentinela/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinela.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
