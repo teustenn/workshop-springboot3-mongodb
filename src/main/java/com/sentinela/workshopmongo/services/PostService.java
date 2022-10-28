86465e2b121bb32aba817a6b35b7c07484ce1667
teustenn <teustenn@gmail.com>
2022-10-28 00:00:35 -0300

    Post findById

---
+++ src/main/java/com/sentinela/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.sentinela.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sentinela.workshopmongo.domain.Post;
+import com.sentinela.workshopmongo.repositories.PostRepository;
+import com.sentinela.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object NOT FOUND."));
+	}
+	
+}
